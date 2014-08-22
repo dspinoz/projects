be3cf27c715276c7ec6cb0fffb9ab61d34a8e61e
Daniel Spinozzi <dspinozzi@csc.com.au>
2014-08-22 17:30:14 +0930

    attempt to get disk and net stats

--- cube-emitters/psutil-emitter.py
+++ cube-emitters/psutil-emitter.py
@@ -1,5 +1,6 @@
 #!/usr/bin/env python
 
+import os
 import psutil
 import json
 import httplib
@@ -9,19 +10,45 @@ CUBEPORT = 1080
 
 mycpu_times = [];
 for cpu in psutil.cpu_times(percpu=True):
-  # TBD more on linux
-  mycpu_times.append({'user': cpu.user, 'system': cpu.system, 'idle': cpu.idle})
+  mycpu_times.append(cpu.__dict__)
 
 mycpu_times_percent = []
 for cpu in psutil.cpu_times_percent(interval=None, percpu=True):
-  # TBD more on linux
-  mycpu_times_percent.append({
-    'user': cpu.user,
-    'system': cpu.system,
-    'idle': cpu.idle
-  })
+  mycpu_times_percent.append(cpu.__dict__)
 
-d = [{'type': 'psutil', 
+mydisk_partitions = []
+mydisk_usage = []
+for part in psutil.disk_partitions():
+  if os.name == 'nt':
+    if 'cdrom' in part.opts or part.fstype == '':
+      # skip cd-rom drives with no disk in it; they may raise
+      # ENOENT, pop-up a Windows GUI error for a non-ready
+      # partition or just hang.
+      continue
+  mydisk_partitions.append(part.__dict__)
+  mydisk_usage.append(psutil.disk_usage(part.mountpoint).__dict__)
+
+mydisk_io_counters = []
+for disk in psutil.disk_io_counters(perdisk=True):
+  if os.name != 'nt':
+    mydisk_io_counters.append(disk.__dict__)
+
+mynet_io_counters = []
+for nic in psutil.net_io_counters(pernic=True):
+  if os.name != 'nt':
+    mynet_io_counters.append(nic.__dict__)
+
+
+d = [{
+     'type': 'psutil',
+     'data': {
+       'plugin': 'os',
+       'value': {
+         'name': os.name
+       }
+     }
+    },
+    {'type': 'psutil', 
       'data': { 
         'plugin' : 'cpu_times',
         'value': mycpu_times
@@ -53,6 +80,41 @@ d = [{'type': 'psutil',
       'data': {
         'plugin': 'virtual_memory',
 	'value': psutil.virtual_memory().__dict__
+      }
+    },
+    {
+      'type': 'psutil',
+      'data': {
+        'plugin': 'swap_memory',
+	'value': psutil.swap_memory().__dict__
+      }
+    },
+    {
+      'type': 'psutil',
+      'data': {
+        'plugin': 'disk_partitions',
+	'value': mydisk_partitions
+      }
+    },
+    {
+      'type': 'psutil',
+      'data': {
+        'plugin': 'disk_usage',
+	'value': mydisk_usage
+      }
+    },
+    {
+      'type': 'psutil',
+      'data': {
+        'plugin': 'disk_io_counters',
+	'value': mydisk_io_counters
+      }
+    },
+    {
+      'type': 'psutil',
+      'data': {
+        'plugin': 'net_io_counters',
+	'value': mynet_io_counters
       }
     }]
 
