4ee2fc6951a3657f80ecc9df737b36b06c46567d
Daniel Spinozzi <dspinozzi@csc.com.au>
2014-08-22 17:07:59 +0930

    show cpu information for windows platform

--- cube-emitters/psutil-emitter.py
+++ cube-emitters/psutil-emitter.py
@@ -7,10 +7,45 @@ import httplib
 CUBEHOST = "localhost"
 CUBEPORT = 1080
 
+mycpu_times = [];
+for cpu in psutil.cpu_times(percpu=True):
+  # TBD more on linux
+  mycpu_times.append({'user': cpu.user, 'system': cpu.system, 'idle': cpu.idle})
+
+mycpu_times_percent = []
+for cpu in psutil.cpu_times_percent(interval=None, percpu=True):
+  # TBD more on linux
+  mycpu_times_percent.append({
+    'user': cpu.user,
+    'system': cpu.system,
+    'idle': cpu.idle
+  })
+
 d = [{'type': 'psutil', 
       'data': { 
         'plugin' : 'cpu_times',
-        'value': psutil.cpu_times().__dict__ 
+        'value': mycpu_times
+      }
+    },
+    {
+      'type': 'psutil',
+      'data': {
+        'plugin': 'cpu_times_percent',
+	'value': mycpu_times_percent
+      }
+    },
+    {
+      'type': 'psutil',
+      'data': {
+        'plugin': 'cpu_percent',
+	'value': psutil.cpu_percent(interval=None, percpu=True)
+      }
+    },
+    {
+      'type': 'psutil',
+      'data': {
+        'plugin': 'cpu_count',
+	'value': psutil.cpu_count()
       }
     }]
 
