b244c8ec26a8a0c9f06652bc645bc40a38a59eb0
Daniel Spinozzi (Raspberry Pi) <dspinoz@gmail.com>
2014-10-03 11:31:01 +0100

    show fix positions

--- raspberrypi/gps.js
+++ raspberrypi/gps.js
@@ -5,6 +5,14 @@ var port = new serialport.SerialPort('/dev/ttyAMA0', {
                 baudrate: 57600,
                 parser: serialport.parsers.readline('\r\n')});
 
+console.log('time,lat,latPole,lon,lonPole,numSat');
+
 port.on('data', function(line) {
-    console.log(nmea.parse(line));
+    var gps = nmea.parse(line);
+    
+    if (gps.type == 'fix')
+    {
+      console.log(gps.timestamp + ',' + gps.lat + ',' +  gps.latPole + ',' + gps.lon + ',' + gps.lonPole + ',' + gps.numSat);
+    }
+    
 });
