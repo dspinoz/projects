b970e028d12da803e35d3fa9cabf70d8933fc861
Daniel <dspinoz@gmail.com>
2014-10-17 00:16:07 +1030

    allow disabling of submit and printing - useful for debugging

--- cube-emitters/cube_emitter.pm
+++ cube-emitters/cube_emitter.pm
@@ -14,6 +14,9 @@ use constant {
    CUBE_PORT => 1080
 };
 
+my $disable_submit = 0;
+my $disable_print = 0;
+
 sub get_lines($)
 {
   my $file = shift;
@@ -24,12 +27,33 @@ sub get_lines($)
   return @lines;
 }
 
+sub disable_submit()
+{
+  $disable_submit = 1;
+}
+sub disable_print()
+{
+  $disable_print = 1;
+}
+
 sub submit($)
 {
   my $ref = shift;
   
   # TBD verbose mode
+  if ($disable_print)
+  {
+    print "Print disabled\n";
+    return 1;
+  }
+  
   print JSON::to_json($ref, {pretty => 1})."\n";
+
+  if ($disable_submit)
+  {
+    print "Submit disabled\n";
+    return 1;
+  }
 
   my $req = HTTP::Request->new('POST', "http://".CUBE_HOST.":".CUBE_PORT."/1.0/event/put");
   $req->header("Content-type" => "application/json");
