f76642836065fab39abad198d85368cf4116216c
Daniel <dspinoz@gmail.com>
2014-11-28 13:51:47 +1030

    modified to accept more args.

--- rhel6/package_download.sh
+++ rhel6/package_download.sh
@@ -1,18 +1,19 @@
 #!/bin/sh
 
-if [ $# -le 1 ]
-then
-  echo "`basename $0` repo [options] package[s]"
+usage() {
+  echo "`basename $0` -r repo [options] -p package"
   echo
   echo "Must provide a repository and package name" 
   echo
-  echo "Some useful options:"
-  echo "  --destdir=<path> : specify download path. Default cwd"
-  echo "  --archlist=<arch> : specify CPU architecture"
-  echo "  --source : get source packages"
-  echo "  --config=<cfg> : specify repo configuration file"
-  exit 1
-fi
+  echo "Options:"
+  echo -e "  -r repo\tRepository to get packages from"
+  echo -e "  -d dir\tDirectory to write packages"
+  echo -e "  -a arch\tSpecify platform architecture"
+  echo -e "  -c cfg\tYum configuration file"
+  echo -e "  -p pack\tRPM package to download"
+  echo -e "  -R\t\tAlso download required dependencies"
+  echo -e "  -S\t\tDownload source packages"
+}
 
 if [ -z "`which yumdownloader`" ]
 then
@@ -21,10 +22,100 @@ then
   exit 1
 fi
 
-REPO=$1
-shift
+PACKS=( )
+REPOS=( )
+ARCH=
+CONFIGS=( )
+SOURCE=0
+REQUIRES=0
+DESTDIR=
 
-yumdownloader --disablerepo=\* --enablerepo=$REPO --resolve $*
-exit $?
+while getopts "hSRd:a:c:r:p:" opt; do
+  case $opt in
+    h) usage; exit 0;;
+    p) PACKS=( ${PACKS[@]} $OPTARG );;
+    r) REPOS=( ${REPOS[@]} $OPTARG );;
+    c) CONFIGS=( ${CONFIGS[@]} $OPTARG );;
+    a) ARCH=$OPTARG;;
+    d) DESTDIR=$OPTARG;;
+    S) SOURCE=1;;
+    R) REQUIRES=1;;
+    *) usage; exit 1;;
+  esac
+done
+
+if [[ ${#REPOS[@]} -eq 0 || ${#PACKS[@]} -eq 0 ]]
+then
+  echo "Must provide at least one repository and one package"
+  echo 
+  usage
+  exit 1
+fi
+
+# BUILD ARGS FOR DOWNLOADING PACKAGES
+
+OPTS=""
+
+for c in ${CONFIGS[@]}
+do
+  OPTS="$OPTS --config=$c"
+done
+
+for r in ${REPOS[@]}
+do
+  OPTS="$OPTS --enablerepo=$r"
+done
+
+if [ -n "$ARCH" ]
+then
+  OPTS="$OPTS --archlist=$ARCH"
+fi
+
+if [ -n "$DESTDIR" ]
+then
+  OPTS="$OPTS --destdir=$DESTDIR"
+fi
+
+if [ $REQUIRES -eq 1 ]
+then
+  OPTS="$OPTS --resolve"
+fi
+
+if [ $SOURCE -eq 1 ]
+then
+  OPTS="$OPTS --source"
+fi
+
+for p in ${PACKS[@]}
+do
+  OPTS="$OPTS $p"
+done
+
+yumdownloader --disablerepo=\* $OPTS 
+yumdlexit=$?
+
+if [ $SOURCE -eq 0 ]
+then
+  exit $yumdlexit
+fi
+
+# TODO CONTINUE TO DOWNLOAD SOURCE PACKAGES
+
+
+exit $yumdlexit
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
