8a18a22e9b182685e0e9664e9255933e191e9485
root <root@csc-reposync.local>
2015-03-05 10:12:52 +1030

    show hooks

---
+++ reposync/reposync2.py
@@ -0,0 +1,28 @@
+from yum.plugins import PluginYumExit, TYPE_CORE, TYPE_INTERACTIVE
+
+requires_api_version = '2.3'
+plugin_type = (TYPE_CORE, TYPE_INTERACTIVE)
+
+def init_hook(conduit):
+	#conduit.info(2, 'hello world')
+	print "DS INIT"
+
+def postreposetup_hook(conduit):
+	#conduit.info(2', 'got repos')
+	print "DS REPO SETUP"
+
+def predownload_hook(conduit):
+	#conduit.info(2, 'downloading...')
+	print "DS PREDOWN"
+
+def postdownload_hook(conduit):
+	#conduit.info(2, 'downloaded.')
+	print "DS POSTDOWN"
+
+def posttrans_hook(conduit):
+	print "DS TRANS DONE"
+
+def close_hook(conduit):
+	print "DS CLOSE"
+
+
