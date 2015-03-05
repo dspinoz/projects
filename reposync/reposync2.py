952cf0c10de37779c0ba7bac9a2d07e137ea41e0
root <root@csc-reposync.local>
2015-03-05 11:42:39 +1030

    verify packages have been downloaded fully

--- reposync/reposync2.py
+++ reposync/reposync2.py
@@ -4,7 +4,11 @@ import os.path
 requires_api_version = '2.3'
 plugin_type = (TYPE_CORE)
 pre_packages = []
-new_packages = []
+
+def package_is_valid(pack):
+	if os.path.isfile(pack.localPkg()) == True and os.path.getsize(pack.localPkg()) == int(pack.returnSimple('packagesize')):
+		return True
+	return False
 
 def init_hook(conduit):
 	#conduit.info(2, 'hello world')
@@ -12,29 +16,30 @@ def init_hook(conduit):
 
 def postreposetup_hook(conduit):
 	#conduit.info(2', 'got repos')
-	print "DS REPO SETUP"
+	print "DS REPO SETUP", conduit.getRepos().listEnabled()
 
 def predownload_hook(conduit):
 	#conduit.info(2, 'downloading...')
 	print "DS PREDOWN", len(pre_packages)
 	for pack in conduit.getDownloadPackages():
-		if os.path.isfile(pack.localPkg()) == False:
+		if package_is_valid(pack) != True:
 			pre_packages.append(pack)
 	print "DS PREDOWN", len(pre_packages), "TO FETCH"
 
 def postdownload_hook(conduit):
 	#conduit.info(2, 'downloaded.')
-	print "DS POSTDOWN", len(new_packages)
-	for pack in pre_packages:
-		if os.path.isfile(pack.localPkg()) == True:
-			new_packages.append(pack)
-			print "DS DOWNLOADED", pack.localPkg()
-	print "DS POSTDOWN DONE", len(new_packages)
+	print "DS POSTDOWN", len(conduit.getDownloadPackages())
 
 def posttrans_hook(conduit):
 	print "DS TRANS DONE"
 
 def close_hook(conduit):
+	new_packages = []
 	print "DS CLOSE", len(new_packages), " FETCHED"
+	for pack in pre_packages:
+		if package_is_valid(pack) == True:
+			new_packages.append(pack)
+			print "DS DOWNLOADED", pack.localPkg()
+	print "DS POSTDOWN DONE", len(new_packages)
 
 
