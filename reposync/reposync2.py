a9bff6c36e1ea25e325323dc9fb6ff1c14bed581
root <root@csc-reposync.local>
2015-03-05 11:04:36 +1030

    provide diff of packages that have been downloaded

--- reposync/reposync2.py
+++ reposync/reposync2.py
@@ -1,7 +1,10 @@
 from yum.plugins import PluginYumExit, TYPE_CORE
+import os.path
 
 requires_api_version = '2.3'
 plugin_type = (TYPE_CORE)
+pre_packages = []
+new_packages = []
 
 def init_hook(conduit):
 	#conduit.info(2, 'hello world')
@@ -13,17 +16,25 @@ def postreposetup_hook(conduit):
 
 def predownload_hook(conduit):
 	#conduit.info(2, 'downloading...')
-	print "DS PREDOWN"
+	print "DS PREDOWN", len(pre_packages)
+	for pack in conduit.getDownloadPackages():
+		if os.path.isfile(pack.localPkg()) == False:
+			pre_packages.append(pack)
+	print "DS PREDOWN", len(pre_packages), "TO FETCH"
 
 def postdownload_hook(conduit):
 	#conduit.info(2, 'downloaded.')
-	for pack in conduit.getDownloadPackages():
-		print "DS DOWNLOADED", pack.localPkg()
+	print "DS POSTDOWN", len(new_packages)
+	for pack in pre_packages:
+		if os.path.isfile(pack.localPkg()) == True:
+			new_packages.append(pack)
+			print "DS DOWNLOADED", pack.localPkg()
+	print "DS POSTDOWN DONE", len(new_packages)
 
 def posttrans_hook(conduit):
 	print "DS TRANS DONE"
 
 def close_hook(conduit):
-	print "DS CLOSE"
+	print "DS CLOSE", len(new_packages), " FETCHED"
 
 
