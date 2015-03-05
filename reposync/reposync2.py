9da36bc9e8fb790fcd903a0d1190f82a798a0f32
root <root@csc-reposync.local>
2015-03-05 12:16:11 +1030

    config file specifies options for plugin

--- reposync/reposync2.py
+++ reposync/reposync2.py
@@ -1,7 +1,8 @@
+from yum.constants import *
 from yum.plugins import PluginYumExit, TYPE_CORE
 import os.path
 
-requires_api_version = '2.3'
+requires_api_version = '2.6'
 plugin_type = (TYPE_CORE)
 pre_packages = []
 
@@ -12,7 +13,14 @@ def package_is_valid(pack):
 
 def init_hook(conduit):
 	#conduit.info(2, 'hello world')
-	print "DS INIT"
+	print "DS INIT FOO", conduit.confBool('main','foo',False) == True
+
+def config_hook(conduit):
+	if hasattr(conduit.getOptParser, 'add_option'):
+		# Command Options cannot be added to reposync
+		conduit.getOptParser().add_option('','--foo', dest='foo',
+			action='store_true', default=False,
+			help='Fooooooooo')
 
 def postreposetup_hook(conduit):
 	#conduit.info(2', 'got repos')
