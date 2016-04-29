d451e499d23a835df1b5b75d2f36fbd032ad732d
dspinoz <dspinoz@gmail.com>
2016-04-29 12:32:04 +0930

    draw tree in plugin module

--- calendarview/mytreeChart.js
+++ calendarview/mytreeChart.js
@@ -1,14 +1,116 @@
 dc.mytreeChart = function (parent, chartGroup) {
-    var _chart = dc.colorMixin(dc.marginMixin(dc.baseMixin({})));
+  var _chart = dc.colorMixin(dc.marginMixin(dc.baseMixin({})));
+  
+  var _data, _max;
 
-    _chart._doRender = function () {
-        _chart.resetSvg();
-        return _chart;
-    };
+  _chart._doRender = function () {
+    _chart.resetSvg();
+    console.log('mytree render');
+    
+    var margin = {top: 10, right: 20, bottom: 20, left: 20},
+        _width = _chart.width() - margin.right - margin.left,
+        _height = _chart.height() - margin.top - margin.bottom;
 
-    _chart._doRedraw = function () {
-        return _chart;
-    };
+    var tree = d3.layout.tree()
+      .size([_width, _height])
+      .children(function(d) { return Array.isArray(d.values) ? d.values : []; });
 
-    return _chart.anchor(parent, chartGroup);
+    var catTree = _chart.svg()
+        .attr("width", _width + margin.right + margin.left)
+        .attr("height", _height + margin.top + margin.bottom)
+      .append("g")
+        .attr("transform", "translate(" + margin.left + "," + margin.top + ")");
+      
+    
+    var color = d3.scale.category10();
+    var rScale = d3.scale.linear().domain([0,_max]).range([2,30]);
+    var diag = d3.svg.diagonal()
+      .projection(function(d) { return [d.x, d.y]; });
+    
+    var nodes = tree.nodes(_data);
+    
+    var links = catTree.selectAll('path.link').data(tree.links(nodes));
+    links.exit().remove();
+    links.enter().append('path').attr('class', 'link');
+    links.transition().attr('d', diag);
+    
+    var g = catTree.selectAll('g.node').data(nodes);
+    
+    g.exit().remove();
+    g.enter().append('g').attr('class', 'node')
+      .on('mouseover', function() {
+        var d = d3.select(this).datum();
+        console.log(d);
+      })
+      .on('click', function() {
+        var d = d3.select(this).datum();
+        console.log('tree click', d);
+        
+        d3.select(this).select('circle').classed('selected', d._selected = !d._selected);
+        
+        if (!d.parent) {
+          //on the root node, reset filters
+          catPie.filter(null);
+          subcatPie.filter(null);
+          
+          g.select('circle').classed('selected', function(d) {
+            d._selected = false;
+            return d._selected; 
+          });
+        }
+        else {
+          
+          if (d.parent.key == "Root") {
+            //apply category filter
+            catPie.filter(d.key);
+          }
+          else {
+            // apply category and subcategory filter
+            catPie.filter(d.parent.key);
+            subcatPie.filter(d.key);
+          }
+        }
+        
+        dc.redrawAll();
+      });
+    g.transition().attr('transform', function(d) { return 'translate('+d.x+','+d.y+')'; });
+    
+    var c = g.selectAll('circle').data(function(d) { return [d]; });
+    c.exit().remove();
+    c.enter().append('circle').attr('r', rScale(1));
+    c.attr('fill', function(d) { return color(d.key); })
+    c.attr('stroke', function(d) { return color(d.key); })
+      .attr('r', function(d) { 
+        if (Array.isArray(d.values)) {
+          return rScale(1); 
+        }
+        return rScale(d.values);
+      });
+    
+    
+    return _chart;
+  };
+
+  _chart._doRedraw = function () {
+    console.log('mytree redraw');
+    return _chart;
+  };
+
+  _chart.data = function (data) {
+    if (!arguments.length) {
+      return _data;
+    }
+    _data = data;
+    return _chart;
+  };
+  
+  _chart.max = function (max) {
+    if (!arguments.length) {
+      return _max;
+    }
+    _max = max;
+    return _chart;
+  };
+
+  return _chart.anchor(parent, chartGroup);
 };
