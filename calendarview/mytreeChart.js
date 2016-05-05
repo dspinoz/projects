5eab06277bb37573dd7a7c3c6d8801ed22fb0978
dspinoz <dspinoz@gmail.com>
2016-05-05 23:30:00 +0930

    tree interacts with dimension

--- calendarview/mytreeChart.js
+++ calendarview/mytreeChart.js
@@ -1,22 +1,28 @@
 dc.mytreeChart = function (parent, chartGroup) {
   var _chart = dc.colorMixin(dc.marginMixin(dc.baseMixin({})));
   
-  _chart._mandatoryAttributes([]);
+  _chart._mandatoryAttributes(['dimension']);
   
-  var _data, _treeG, _treeData;
+  _chart.colorAccessor(function(d) { return d.key; });
+  
+  var _treeG;
   
   var _tree = d3.layout.tree()
     .children(function(d) { return Array.isArray(d.values) ? d.values : []; });
     
   var _nest = d3.nest();
+  var _title = function(d) { return d; };
+  var _filterFunc = function(d) { return d; };
   
   var _diagonal = d3.svg.diagonal().projection(function(d) { return [d.x, d.y]; });
-  var _color = d3.scale.category10();
   var _scale = d3.scale.linear().range([0,50]);
   
   _chart.drawTreeNodes = function() {
     
-    var nodes = _tree.nodes(_treeData);
+    // new data is regenerated every refresh - not persistent!!
+    var treeData = _chart.dataRoot(_nest.entries(_chart.dimension().top(Infinity)));
+    
+    var nodes = _tree.nodes(treeData);
     
     var links = _treeG.selectAll('path.link').data(_tree.links(nodes));
     links.exit().remove();
@@ -28,43 +34,28 @@ dc.mytreeChart = function (parent, chartGroup) {
     g.exit().remove();
     g.enter().append('g').attr('class', 'node')
       .on('click', function() {
+        // TBD save state for what item is selected and maintain selection as data changes
+        
         var d = d3.select(this).datum();
-        console.log('tree click', d, d3.select(this).attr('class'));
         
-        d3.select(this).select('circle').classed('selected', d._selected = !d._selected);
-        
-        if (!d.parent) {
-          //on the root node, reset filters
-          catPie.filter(null);
-          subcatPie.filter(null);
-          
-          g.select('circle').classed('selected', function(d) {
-            d._selected = false;
-            return d._selected; 
-          });
-        }
-        else {
-          
-          if (d.parent.key == "Root") {
-            //apply category filter
-            catPie.filter(d.key);
-          }
-          else {
-            // apply category and subcategory filter
-            catPie.filter(d.parent.key);
-            subcatPie.filter(d.key);
-          }
-        }
+        _chart.filter(_filterFunc(d));
         
         dc.redrawAll();
       });
+      
     g.transition().attr('transform', function(d) { return 'translate('+d.x+','+d.y+')'; });
+    
+    var t = g.selectAll('title').data(function(d) { return [d]; });
+    t.exit().remove();
+    t.enter().append('title');
+    t.text(_chart.title());
     
     var c = g.selectAll('circle').data(function(d) { return [d]; });
     c.exit().remove();
     c.enter().append('circle').attr('r', _scale(1));
-    c.attr('fill', function(d) { return _color(d.key); })
-    c.attr('stroke', function(d) { return _color(d.key); })
+    c
+      .attr('fill', _chart.getColor)
+      .attr('stroke', _chart.getColor)
       .attr('r', function(d) { 
         if (Array.isArray(d.values)) {
           return _scale(1); 
@@ -87,8 +78,6 @@ dc.mytreeChart = function (parent, chartGroup) {
         .attr("height", height + _chart.margins().top + _chart.margins().bottom)
       .append("g")
         .attr("transform", "translate(" + _chart.margins().left + "," + _chart.margins().top + ")");
-    
-    _treeData = _chart.dataRoot(_nest.entries(_data));
     
     _chart.drawTreeNodes();
     
@@ -124,6 +113,22 @@ dc.mytreeChart = function (parent, chartGroup) {
       return _tree.children();
     }
     _tree.children(f);
+    return _chart;
+  };
+  
+  _chart.title = function (f) {
+    if (!arguments.length) {
+      return _title;
+    }
+    _title = f;
+    return _chart;
+  };
+
+  _chart.filterFunc = function (f) {
+    if (!arguments.length) {
+      return _filterFunc;
+    }
+    _filterFunc = f;
     return _chart;
   };
 
