9269e84bdf1ba17e07bb99c6ea758a14f1f3aeac
dspinoz <dspinoz@gmail.com>
2017-05-11 07:46:17 +0930

    dynamic modes

--- lightworks/main.py
+++ lightworks/main.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python
 import sys
 from optparse import OptionParser
+from importlib import import_module
 
 import config
 
@@ -10,6 +11,7 @@ if __name__ == '__main__':
   parser.add_option("-c", "--config-print", dest="config", action="store_true", help="Print current configuration options")
   parser.add_option("", "--config-key", dest="config_key", help="Show value for config option")
   parser.add_option("", "--config-value", dest="config_value", help="Set value for config-key")
+  parser.add_option("-m", "--mode", dest="mode", default=None, help="Mode to run")
 
   (options,args) = parser.parse_args()
 
@@ -20,14 +22,34 @@ if __name__ == '__main__':
   if options.config:
     for c in config.list():
       print "{} = {}".format(c[0],c[1])
+    sys.exit(0)
 
   if options.config_value and options.config_key:
     config.set(options.config_key, options.config_value)
+    sys.exit(0)
 
   if options.config_key:
     (k,v) = config.get(options.config_key)
     if k is None:
       print "Invalid option, {}".format(options.config_key)
+      sys.exit(1)
     else:
       print "{} = {}".format(k,v)
+      sys.exit(0)
       
+  if options.mode is None:
+    print parser.format_help()
+    sys.exit(0)
+
+  mode = None
+  try:
+    mode = import_module("mode.{}".format(options.mode))
+  except ImportError as e:
+    print "Invalid mode, {}: {}".format(options.mode, e)
+    sys.exit(1)
+
+  print mode
+
+
+
+
