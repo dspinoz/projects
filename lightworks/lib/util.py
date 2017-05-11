972d95cea147adf53905a93561999d98594c9c5e
dspinoz <dspinoz@gmail.com>
2017-05-11 22:37:16 +0930

    more utils

--- lightworks/lib/util.py
+++ lightworks/lib/util.py
@@ -18,3 +18,31 @@ def ffmpeg_print_output(queue,proc):
 			identifier,line = it
 			print identifier + ':', line
 				
+
+def progress(count, total, status=''):
+    bar_len = 60
+    filled_len = int(round(bar_len * count / float(total)))
+
+    percents = round(100.0 * count / float(total), 1)
+    bar = '=' * filled_len + '-' * (bar_len - filled_len)
+
+    sys.stdout.write('[%s] %s%s ...%s\r' % (bar, percents, '%', status))
+    sys.stdout.flush()
+
+def find_media(search='raw', exts=['.MOV']):
+	ret = []
+	for root,dirs,files in os.walk(search):
+		
+		for f in files:
+			if f.startswith("."):
+				continue
+			for ext in exts:
+				if f.endswith(ext):
+					p = os.path.join(root,f)
+					if p.startswith(search+"/"):
+						ret.append(p[len(search)+1:])
+					elif p.startswith(search):
+						ret.append(p[len(search):])
+	return ret
+
+
