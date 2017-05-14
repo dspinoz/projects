1271649e273e062330ae620d88a05fe864472724
dspinoz <dspinoz@gmail.com>
2017-05-14 19:12:37 +0930

    specify mode on import

--- lightworks/mode/import.py
+++ lightworks/mode/import.py
@@ -13,6 +13,7 @@ The following metadata fields are available:
   
   size      number of bytes contained in the file
   mtime     last time file was modified
+  mode      0 RAW, 1 PROXY, 2 SCALED
 
 """
 
@@ -20,12 +21,16 @@ def get_parser():
   parser = optparse.OptionParser(add_help_option=False, description=desc, usage=optparse.SUPPRESS_USAGE)
   parser.add_option('-h', "--help", dest="help", action="store_true", help="Show import options")
   parser.add_option("-p", "--path", dest="path", help="Path to files to import")
+  parser.add_option("-m", "--mode", dest="mode", help="Current mode of files to import", default="RAW", choices=["RAW", "PROXY", "SCALED"])
   return parser
   
 def parser_hook(parser,options,args):
   if options.help:
     print parser.format_help()
     sys.exit(0)
+
+  if options.mode:
+    options.mode = getattr(db.FileMode, options.mode)
     
   if options.path:
     if not os.path.exists(options.path):
@@ -33,17 +38,17 @@ def parser_hook(parser,options,args):
       sys.exit(1)
     if os.path.isdir(options.path):
       print "Importing directory",options.path
-      import_directory(options.path)
+      import_directory(options.path, options.mode)
     else:
       print "Importing file", options.path
-      import_file(options.path)
+      import_file(options.path, options.mode)
     
     sys.exit(0)
       
   print parser.format_help()
   sys.exit(0)
   
-def import_file(path):
+def import_file(path,mode):
 
   os.stat_float_times(False)
   st = os.stat(path)
@@ -51,7 +56,7 @@ def import_file(path):
   stats = []
   stats.append(('size',st.st_size))
   stats.append(('mtime',st.st_mtime))
-  stats.append(('mode',db.FileMode.RAW))
+  stats.append(('mode',mode))
 
   list = db.add(path)
   if list is not None and len(list) is 1:
@@ -64,7 +69,7 @@ def import_file(path):
   else:
     print "ERROR importing",path
 
-def import_directory(path):
+def import_directory(path,mode):
 
   for root,dirs,files in os.walk(path):
     for f in files:
@@ -72,5 +77,5 @@ def import_directory(path):
         continue
       
       p = os.path.join(root,f)
-      import_file(p)
+      import_file(p, mode)
     
