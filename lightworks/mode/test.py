9393c3e4c59d70e4a4f0ef0dc1cf33e3087282ca
Daniel Spinozzi <dspinoz@gmail.com>
2017-05-16 10:11:09 +0930

    test mode

--- lightworks/mode/test.py
+++ lightworks/mode/test.py
@@ -1,4 +1,24 @@
-from lib import mode
+from datetime import datetime
+import sys
+import os
+import optparse
 
-def a():
-  print "--"
+import lib.util as util
+import lib.lwdb_file as db
+
+desc="""
+Test mode.
+"""
+
+def get_parser():
+  parser = optparse.OptionParser(add_help_option=False, description=desc, usage=optparse.SUPPRESS_USAGE)
+  parser.add_option('', "--help", dest="help", action="store_true", help="Show ls options")
+  return parser
+  
+def parser_hook(parser,options,args):
+  if options.help:
+    print parser.format_help()
+    sys.exit(0)
+  
+  sys.exit(0)
+  
