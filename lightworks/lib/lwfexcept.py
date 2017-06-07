a2b7b6010d44a412e82afafdf1e7147a12a0f985
dspinoz <dspinoz@gmail.com>
2017-06-07 13:55:44 +0930

    add more exceptions

--- lightworks/lib/lwfexcept.py
+++ lightworks/lib/lwfexcept.py
@@ -1,4 +1,26 @@
 
+# LWF project has not been initialised
 class LWFNotInitialisedError(Exception):
+  def __init__(self):
+    Exception.__init__(self)
+
+# project file has already been defined
+class ProjectFileAlreadyExistsError(Exception):
+  def __init__(self):
+    Exception.__init__(self)
+
+# file has already been imported
+class FileAlreadyImportedError(Exception):
+  def __init__(self):
+    Exception.__init__(self)
+
+# file has already been imported and is stored on another project_file
+class ProjectFileDuplicateFileError(Exception):
+  def __init__(self):
+    Exception.__init__(self)
+
+# attempt to import a file as a project_file, but the mode has already 
+# been consumed by another file
+class ProjectFileModeAlreadyTakenError(Exception):
   def __init__(self):
     Exception.__init__(self)
