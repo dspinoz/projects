3d09eedc014cdc9ae082b69717dae12b52b2a023
dspinoz <dspinoz@gmail.com>
2017-06-14 08:09:55 +0930

    run ffmpeg to intermediate

--- lightworks/lib/worker.py
+++ lightworks/lib/worker.py
@@ -2,6 +2,8 @@ import threading
 import time
 
 import lib.db.queue as db
+import lib.db.file as fdb
+import lib.ffmpeg as ffmpeg
 
 class Thread(threading.Thread):
 
@@ -9,6 +11,7 @@ class Thread(threading.Thread):
     threading.Thread.__init__(self)
     self._stop_event = threading.Event()
     self.num = num
+    self.ffmpeg = None
     print "worker thread {} create".format(num)
 
   def run(self):
@@ -17,7 +20,22 @@ class Thread(threading.Thread):
       print "go {} ".format(self.num)
       try:
         c = db.pop()
-        print "{} :{} #{} {} {} {}".format(self.num, c[0],c[1]['file'],c[1]['type'],c[1]['from'],c[1]['to'])
+        #print "{} :{} #{} {} {} {}".format(self.num, c[0],c[1]['file'],c[1]['type'],c[1]['from'],c[1]['to'])
+        
+        if c[1]['type'] == 'transcode':
+          f = fdb.list(id=c[1]['file'])[0]
+          script = None
+          print "TRANSCODE {}".format(f.path)
+          if c[1]['to'] is fdb.FileMode.INTERMEDIATE and c[1]['from'] is fdb.FileMode.RAW:
+            script = 'scripts/intermediate-h264'
+
+          if script is not None:
+            self.ffmpeg = ffmpeg.FFMPEG(script, f.path)
+ 
+            self.ffmpeg.start()
+
+            self.ffmpeg.wait()
+        
       except IndexError:
         for i in range(5):
           if not self.stopped():
@@ -25,7 +43,11 @@ class Thread(threading.Thread):
     print "worker {} done".format(self.num)
 
   def kill(self):
+    print "worker {} kill".format(self.num)
     self._stop_event.set()
+    if self.ffmpeg:
+      self.ffmpeg.kill()
+      self.ffmpeg.wait()
 
   def stopped(self):
     return self._stop_event.is_set()
