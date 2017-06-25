8cd158d6aa59468f12a344deecd34e253242addf
dspinoz <dspinoz@gmail.com>
2017-06-26 08:28:54 +0930

    generic class for switching modes

--- lightworks/lib/lwf_file.py
+++ lightworks/lib/lwf_file.py
@@ -10,94 +10,88 @@ import lib.db.config as cfg
 import lib.db.file as fdb
 import lib.db.project_file as pdb
 
-desc="""
-Switch to proxy files.
-
-"""
-
-def get_parser():
-  parser = optparse.OptionParser(add_help_option=False, description=desc, usage=optparse.SUPPRESS_USAGE)
-  parser.add_option('', "--help", dest="help", action="store_true", help="Show proxy options")
-  parser.add_option("-l", "--list", dest="list_proxy", action="store_true", help="List proxy files")
-  return parser
-  
-def parser_hook(parser,options,args):
-  if options.help:
-    print parser.format_help()
-    sys.exit(0)
- 
-  list = []
-
-  try:
-    list = pdb.list()
-  except lwfexcept.ProjectFileNotFoundError:
-    pass
-
-  for p in list:
-    p.fetch()
-
-    f = p.get(fdb.FileMode.PROXY)
-
-    if os.path.exists(p.path):
-      print "remove existing file, mode",p.mode,"path",p.path
-      # todo unlink if safe
-    p.set_mode(None)
-
-    if f is None:
-      print p.path,"????"
-      continue
-
-    print p.path,f.path
-
-    copyfile = u.str2bool(cfg.get("proxycopy")[1])
-    datadir = os.path.join(lwf.data_dir(),cfg.get("proxydir")[1])
-
-    rpath = u.strip_path_components(f.path,safe_root=True)
-    fpath = os.sep.join([datadir,rpath])
-    
-    if not copyfile:
-      if not os.path.exists(f.path):
-        print "File has gone! No copy available"
-        continue
-      else:
-        fpath = f.path
+class LWFFile:
+  def __init__(self,mode):
+    if mode == fdb.FileMode.PROXY:
+      self.mode = fdb.FileMode.PROXY
+      self.modestr = "proxy"
     else:
-      if not os.path.exists(fpath):
-        print "File copy has gone!"
+      raise lwfexcept.UnsupportedFileModeError
+    
+  def set(self):
+ 
+    list = []
+
+    try:
+      list = pdb.list()
+    except lwfexcept.ProjectFileNotFoundError:
+      pass
+
+    for p in list:
+      p.fetch()
+
+      f = p.get(self.mode)
+
+      if os.path.exists(p.path):
+        print "remove existing file, mode",p.mode,"path",p.path
+        # todo unlink if safe
+        
+      p.set_mode(None)
+
+      if f is None:
+        print p.path,"????"
         continue
+
+      print p.path,f.path
+
+      copyfile = u.str2bool(cfg.get("{}copy".format(self.modestr))[1])
+      datadir = os.path.join(lwf.data_dir(),cfg.get("{}dir".format(self.modestr))[1])
+
+      rpath = u.strip_path_components(f.path,safe_root=True)
+      fpath = os.sep.join([datadir,rpath])
+      
+      if not copyfile:
+        if not os.path.exists(f.path):
+          print "File has gone! No copy available"
+          continue
+        else:
+          fpath = f.path
       else:
-        fpath = fpath
+        if not os.path.exists(fpath):
+          print "File copy has gone!"
+          continue
+        else:
+          fpath = fpath
 
 
-    savepath = os.path.join(os.path.dirname(lwf.data_dir()), p.path)
-    u.eprint ("savepath full {}".format(savepath))
-    savepath = os.path.relpath(savepath)
-    u.eprint ("savepath rel {}".format(savepath))
-    savepath = os.path.realpath(savepath)
-    u.eprint ("savepath real {}".format(savepath))
-
-    if os.path.exists(savepath):
-      sst = os.stat(savepath)
-      fst = os.stat(fpath)
-
-      if sst.st_size != fst.st_size:
-        print "not the proxy file, size diff!"
-        continue
-
-      if sst.st_mtime != fst.st_mtime:
-        print "not the proxy file, mtime diff!"
-        continue
+      savepath = os.path.join(os.path.dirname(lwf.data_dir()), p.path)
+      u.eprint ("savepath full {}".format(savepath))
+      savepath = os.path.relpath(savepath)
+      u.eprint ("savepath rel {}".format(savepath))
+      savepath = os.path.realpath(savepath)
+      u.eprint ("savepath real {}".format(savepath))
 
       if os.path.exists(savepath):
-        print savepath,"already here"
-        continue
-    
-    print "copying",fpath,savepath
-    if not os.path.exists(os.path.dirname(savepath)):
-      os.makedirs(os.path.dirname(savepath))
-    
-    shutil.copy2(fpath,savepath)
-    p.set_mode(fdb.FileMode.PROXY)
+        sst = os.stat(savepath)
+        fst = os.stat(fpath)
 
-  sys.exit(0)
+        if sst.st_size != fst.st_size:
+          print "not the {} file, size diff!".format(self.modestr)
+          continue
+
+        if sst.st_mtime != fst.st_mtime:
+          print "not the {} file, mtime diff!".format(self.modestr)
+          continue
+
+        if os.path.exists(savepath):
+          print "{}".format(self.modestr),savepath,"already here"
+          continue
+      
+      print "copying",fpath,savepath
+      if not os.path.exists(os.path.dirname(savepath)):
+        os.makedirs(os.path.dirname(savepath))
+      
+      shutil.copy2(fpath,savepath)
+      p.set_mode(self.mode)
+
   
