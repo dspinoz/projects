77036632775560d5de3c3b9a689580851e1cd851
dspinoz <dspinoz@gmail.com>
2017-09-07 22:10:19 +0930

    show pydoc for class before running test

--- python_testing/example1.py
+++ python_testing/example1.py
@@ -1,8 +1,14 @@
 #!/usr/bin/python
 
 import unittest
+import inspect
 
+# this is a comment
 class TestStringMethods(unittest.TestCase):
+    """This is the start of the test for String methods.
+
+    Some additional information...
+    """
 
     def test_upper(self):
         self.assertEqual('foo'.upper(), 'FOO')
@@ -12,11 +18,20 @@ class TestStringMethods(unittest.TestCase):
         self.assertFalse('Foo'.isupper())
 
     def test_split(self):
+        """Test the split method of string with 'hello' and 'world'
+
+        Some additional info about split...
+        """
         s = 'hello world'
         self.assertEqual(s.split(), ['hello', 'world'])
         # check that s.split fails when the separator is not a string
+
+        self.assertTrue('WOOPA'.isupper(),'WOOPA')
         with self.assertRaises(TypeError):
             s.split(2)
 
 if __name__ == '__main__':
+    print inspect.getdoc(TestStringMethods)
+    print "COMMENTS:"
+    print inspect.getcomments(TestStringMethods)
     unittest.main()
