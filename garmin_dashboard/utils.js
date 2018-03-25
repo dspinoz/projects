64c6643c96b101172f30a8e02728b00106607173
dspinoz <dspinoz@gmail.com>
2018-03-25 12:11:20 +1030

    show total time

---
+++ garmin_dashboard/utils.js
@@ -0,0 +1,17 @@
+
+function formatSeconds(seconds) {
+  var sec = 1;
+  var sec_min = sec * 60;
+  var sec_hour = sec_min * 60;
+  var sec_day = sec_hour * 7.5;
+  var sec_week = sec_day * 5;
+  
+  var weeks = Math.floor(seconds / sec_week);
+  var days = Math.floor((seconds % sec_week) / sec_day);
+  var hours = Math.floor(((seconds % sec_week) % sec_day) / sec_hour);
+  var mins = Math.floor((((seconds % sec_week) % sec_day) % sec_hour) / sec_min);
+  return (weeks ? weeks + "w " : "") +
+         (days ? days + "d " : "") +
+         (hours ? hours + "h " : "") +
+         (mins ? mins + "m " : "");
+}
