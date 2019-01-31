12ae03a31b5ef393bfcd0acb7ce9cdad77a01792
dspinoz <dspinoz@gmail.com>
2019-01-31 21:12:15 +1030

    add inventory to models

--- photoview/aws_glacier/models.py
+++ photoview/aws_glacier/models.py
@@ -4,3 +4,12 @@ from __future__ import unicode_literals
 from django.db import models
 
 # Create your models here.
+
+class AWSGlacierModel(models.Model):
+  accountId = models.CharField(max_length=255)
+  vaultName = models.CharField(max_length=255)
+
+class Inventory(AWSGlacierModel):
+  output = models.TextField()
+  date = models.DateTimeField()
+  
