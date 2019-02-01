680da202c25b2f31217b5079800a1ee03ab9c3f9
dspinoz <dspinoz@gmail.com>
2019-02-01 15:46:30 +1030

    example loading a fixure

--- photoview/aws_glacier/tests.py
+++ photoview/aws_glacier/tests.py
@@ -5,8 +5,6 @@ from django.test import TestCase
 
 from .models import Inventory
 
-
-
 class InventoryDBTests(TestCase):
   def testIsEmpty(self):
     """ check that the database is empty """
@@ -17,7 +15,7 @@ class InventoryDBTests(TestCase):
     self.assertIs(Inventory.objects.count(), 1)
   def testCreateObjectWithOutput(self):
     self.assertIs(Inventory.objects.count(), 0)
-    Inventory.objects.create(output=json.dumps({"ArchiveList":[]}))
+    Inventory.objects.create(output=json.dumps({}))
     self.assertIs(Inventory.objects.count(), 1)
   def testCreateObjectWithDate(self):
     self.assertIs(Inventory.objects.count(), 0)
@@ -25,5 +23,14 @@ class InventoryDBTests(TestCase):
     self.assertIs(Inventory.objects.count(), 1)
   def testCreateObjectWithParameters(self):
     self.assertIs(Inventory.objects.count(), 0)
-    Inventory.objects.create(date=datetime.now(), output=json.dumps({"ArchiveList":[]}))
+    Inventory.objects.create(date=datetime.now(), output=json.dumps({}))
     self.assertIs(Inventory.objects.count(), 1)
+
+
+class LoadDataTest(TestCase):
+  fixtures = ['test-04-archive-recorded']
+  def testData(self):
+    self.assertIs(Inventory.objects.count(), 1)
+    for i in Inventory.objects.all():
+      self.assertNotEqual(i.date, None, "Inventory has date set")
+      self.assertNotEqual(i.output, None, "Inventory has output set")
