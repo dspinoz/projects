2023a438b1a5a5cea380c9eeb49d8c28c484826c
dspinoz <dspinoz@gmail.com>
2019-02-01 15:34:43 +1030

    ensure there is an object added to the collection

--- photoview/aws_glacier/tests.py
+++ photoview/aws_glacier/tests.py
@@ -12,14 +12,18 @@ class InventoryDBTests(TestCase):
     """ check that the database is empty """
     self.assertIs(Inventory.objects.count(), 0)
   def testCreateObject(self):
+    self.assertIs(Inventory.objects.count(), 0)
     Inventory.objects.create()
     self.assertIs(Inventory.objects.count(), 1)
   def testCreateObjectWithOutput(self):
+    self.assertIs(Inventory.objects.count(), 0)
     Inventory.objects.create(output=json.dumps({"ArchiveList":[]}))
     self.assertIs(Inventory.objects.count(), 1)
   def testCreateObjectWithDate(self):
+    self.assertIs(Inventory.objects.count(), 0)
     Inventory.objects.create(date=datetime.now())
     self.assertIs(Inventory.objects.count(), 1)
   def testCreateObjectWithParameters(self):
+    self.assertIs(Inventory.objects.count(), 0)
     Inventory.objects.create(date=datetime.now(), output=json.dumps({"ArchiveList":[]}))
     self.assertIs(Inventory.objects.count(), 1)
