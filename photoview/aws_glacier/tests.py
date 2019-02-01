654bda9863910a572152e111c1cdd3415a0d0544
dspinoz <dspinoz@gmail.com>
2019-02-01 16:43:10 +1030

    test for job and inventory to make an inventory retrieval

--- photoview/aws_glacier/tests.py
+++ photoview/aws_glacier/tests.py
@@ -48,4 +48,47 @@ class LoadDataTest(TestCase):
     for r in InventoryRetrieval.objects.all():
       self.assertNotEqual(r.job, None, "Inventory retrieval has job set")
       self.assertNotEqual(r.inventory, None, "Inventory retrieval has inventory set")
-    
+
+class InventoryDataTest(TestCase):
+  def testInventoryRetrievalJobAndOutput(self):
+    # initiating job from AWS ...
+    self.assertIs(Job.objects.count(), 0, "No jobs recorded yet")
+    Job.objects.create(jobId="aaa")
+    self.assertIs(Job.objects.count(), 1, "Job inserted to database")
+    j = Job.objects.get(jobId="aaa")
+    self.assertNotEqual(j, None, "Job retrieved from database")
+    self.assertIs(j.completed, False, "Job is not completed")
+    self.assertEqual(j.inventoryretrieval_set.count(), 0, "Job has no inventory retrieval recorded")
+    # waiting for job to complete ...
+    j.completed = True
+    j.save()
+    self.assertIs(j.completed, True, "Job is now completed")
+    self.assertIs(j.retrievedOutput, False, "Job has not yet retrieved outputs")
+    #retrieving outputs from AWS ...
+    self.assertIs(Inventory.objects.count(), 0, "No inventories recorded yet")
+    i = Inventory.objects.create(output=json.dumps({"ArchiveList":[]}))
+    self.assertNotEqual(i.date, None, "Inventory has date set")
+    self.assertNotEqual(i.output, None, "Inventory has output set")
+    self.assertIs(Inventory.objects.count(), 1, "Inventory has been recorded")
+    # link outputs to job and log as inventory retrieval
+    j.retrievedOutput = True
+    j.save()
+    self.assertIs(j.retrievedOutput, True, "Job has outputs retrieved")
+    self.assertIs(InventoryRetrieval.objects.count(), 0, "No retrievals recorded yet")
+    r = InventoryRetrieval.objects.create(job=j, inventory=i)
+    self.assertNotEqual(r.job, None, "Retrieval has job")
+    self.assertNotEqual(r.inventory, None, "Retrieval has inventory")
+    self.assertIs(InventoryRetrieval.objects.count(), 1, "Retrievals has been recorded")
+    # check retrievals are recorded in all objects
+    for r in InventoryRetrieval.objects.all():
+      self.assertEqual(r.job.jobId, "aaa", "Retrieval has correct job id")
+      self.assertIs(r.inventory.id, 1, "Retrieval has inventory recorded")
+    for j in Job.objects.all():
+       self.assertEqual(j.jobId, "aaa", "Job id is correct")
+       self.assertEqual(j.inventoryretrieval_set.count(), 1, "Job has inventory retrievals recorded")
+       for r in j.inventoryretrieval_set.all():
+         self.assertEqual(r.id, 1, "Job has Retrieval set")
+         # as job is an inventory retrieval, get the output
+         self.assertNotEqual(r.inventory.output, "", "Inventory for retrieval is set")
+         output = json.loads(r.inventory.output)
+         self.assertEqual(output['ArchiveList'], [], "Inventory has empty archive list")
