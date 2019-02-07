37a1094a800d47dccd2ad83b38ab7a658558e384
dspinoz <dspinoz@gmail.com>
2019-02-07 13:45:56 +1030

    show avaiable jobs

--- photoview/aws_glacier/views/index.py
+++ photoview/aws_glacier/views/index.py
@@ -4,8 +4,12 @@ from __future__ import unicode_literals
 from django.shortcuts import render
 from django.http import HttpResponse
 
+import aws_glacier
+
 def index(request):
   context = {
     'title': 'Glacier',
+    'jobs': aws_glacier.models.Job.objects.filter(available=True),
   }
+  
   return render(request, 'aws_glacier/index.html', context)
