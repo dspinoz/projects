fb663cc9e175c1de44e6ed908a7f88cbb668bee5
dspinoz <dspinoz@gmail.com>
2019-02-07 13:25:36 +1030

    first view

--- photoview/aws_glacier/views/index.py
+++ photoview/aws_glacier/views/index.py
@@ -4,7 +4,8 @@ from __future__ import unicode_literals
 from django.shortcuts import render
 from django.http import HttpResponse
 
-# Create your views here.
-
 def index(request):
-  return HttpResponse("Hello World")
+  context = {
+    'title': 'Glacier',
+  }
+  return render(request, 'aws_glacier/index.html', context)
