5c157b7692b6aa9db4693cb6de684ef59e12baa4
dspinoz <dspinoz@gmail.com>
2019-02-08 14:19:19 +1030

    util for listing collections

--- photoview/aws_rekognition/management/commands/listcollections.py
+++ photoview/aws_rekognition/management/commands/listcollections.py
@@ -1,23 +1,12 @@
 import json
-import dateutil
 
-import boto3
-
-from django.core.management.base import BaseCommand, CommandError
+from django.core.management.base import BaseCommand
 from django.core.exceptions import ObjectDoesNotExist
 
-from aws_rekognition.models import AWSRekognitionRequestResponse
+import aws_rekognition.utils as u
 
 class Command(BaseCommand):
   help = "List Collections"
   
   def handle(self, *args, **options):
-    rek_conn = boto3.client('rekognition')
-    
-    res = rek_conn.list_collections()
-    
-    meta = res['ResponseMetadata']
-    headers = meta['HTTPHeaders']
-    AWSRekognitionRequestResponse.objects.create(requestId=meta['RequestId'], endpoint = 'list_collections', date = dateutil.parser.parse(headers['date']), statusCode = meta['HTTPStatusCode'], retryAttempts = meta['RetryAttempts'], responseLength = headers['content-length'], responseContentType = headers['content-type'], responseBody=json.dumps(res))
-    
-    print(json.dumps(res))
+    print(json.dumps(u.list_collections()))
--- photoview/aws_rekognition/utils/__init__.py
+++ photoview/aws_rekognition/utils/__init__.py
@@ -1,2 +1,3 @@
 
 from .listfaces import *
+from .listcollections import *
