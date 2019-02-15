c13789c35a7d47f842f3409ee25bc7c191a1f2c5
dspinoz <dspinoz@gmail.com>
2019-02-16 01:31:12 +1030

    show the 256px tiles for images in db

--- photoview/aws_rekognition/views/__init__.py
+++ photoview/aws_rekognition/views/__init__.py
@@ -2,3 +2,4 @@
 #from ..models import *
 
 from index import *
+from tiles import *
---
+++ photoview/aws_rekognition/views/tiles.py
@@ -0,0 +1,14 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+
+from django.shortcuts import render
+
+import aws_rekognition
+
+def tiles(request):
+  context = {
+    'title': 'Rekognition',
+    'tiles': aws_rekognition.models.ConvertedImage.objects.filter(metadata__iregex=r'"Type": "thumbnail"').filter(metadata__iregex=r'"Width": 256'),
+  }
+  
+  return render(request, 'aws_rekognition/tiles.html', context)
