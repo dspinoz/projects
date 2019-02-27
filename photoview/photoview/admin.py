4bd41929ff1393e3a520b9f5f99e373cea933de5
dspinoz <dspinoz@gmail.com>
2019-02-27 20:22:32 +1030

    add models from aws_rekognition

--- photoview/photoview/admin.py
+++ photoview/photoview/admin.py
@@ -4,3 +4,38 @@ from __future__ import unicode_literals
 from django.contrib import admin
 
 # Register your models here.
+
+from .models import ConvertedImage
+from .models import IndexedImage
+
+class ConvertedImageInline(admin.TabularInline):
+  model = ConvertedImage
+
+@admin.register(IndexedImage)
+class IndexedImageAdmin(admin.ModelAdmin):
+  date_hierarchy = 'creationDate'
+  list_display = ('id','filePath', 'width', 'height', 'contentType', 'size')
+  list_filter = ('contentType', 'width', 'height')
+  inlines = [ConvertedImageInline]
+
+@admin.register(ConvertedImage)
+class ConvertedImageAdmin(admin.ModelAdmin):
+  date_hierarchy = 'creationDate'
+  list_display = ('id', 'image_id', 'file_name', 'size', 'Type', 'Width', 'file_link')
+  
+  def image_id(self,obj):
+    return obj.orig.id
+  def file_name(self,obj):
+    try:
+      return obj.file.url
+    except ValueError:
+      return "NOT_SET"
+  def file_link(self,obj):
+    try:
+      return format_html(u'<a href="{}">view</a>', obj.file.url)
+    except ValueError:
+      return "NOT_SET"
+  def Type(self,obj):
+    return json.loads(obj.metadata)['Type']
+  def Width(self,obj):
+    return json.loads(obj.metadata)['Width']
--- photoview/photoview/models.py
+++ photoview/photoview/models.py
@@ -1,6 +1,44 @@
 # -*- coding: utf-8 -*-
 from __future__ import unicode_literals
 
+import os
+import json
+from datetime import datetime
+
 from django.db import models
 
 # Create your models here.
+
+class IndexedImage(models.Model):
+  filePath = models.TextField()
+  sha256 = models.CharField(max_length=255)
+  width = models.IntegerField()
+  height = models.IntegerField()
+  size = models.BigIntegerField()
+  contentType = models.CharField(max_length=255)
+  metadata = models.TextField(blank=True, default=None, null=True)
+  creationDate = models.DateTimeField(default=datetime.today)
+  lastModifiedDate = models.DateTimeField(auto_now=True)
+  
+  def fileName(self):
+    return os.path.basename(self.filePath)
+    
+  def getConversions(self):
+    return ConvertedImage.objects.filter(orig=self.id)
+
+class ConvertedImage(models.Model):
+  orig = models.ForeignKey(IndexedImage, models.CASCADE)
+  file = models.ImageField(upload_to='photoview/%Y/%m/%d/')
+  size = models.BigIntegerField()
+  metadata = models.TextField(blank=True, default=None, null=True)
+  creationDate = models.DateTimeField(default=datetime.today)
+  lastModifiedDate = models.DateTimeField(auto_now=True)
+  
+  def metadataObj(self):
+    if self.metadata:
+      return json.loads(self.metadata)
+    return None
+    
+  def getImageCreationDate(self):
+    img = IndexedImage.objects.get(id=self.orig.id)
+    return img.creationDate
